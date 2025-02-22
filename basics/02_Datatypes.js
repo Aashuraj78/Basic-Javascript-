a04e4858f34cbfb552e615b354fc89d7284714a5
Ayush kumar <147746755+Aashuraj78@users.noreply.github.com>
2025-02-22 17:22:58 +0000

    config done

---
+++ basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+let name = "Ankit"
+
+let age = 20
+
+let isloggedIn = false
+
+
+// code readability should be high
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object 
+
+//console.log(null) // object
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "ayushkumar@gmail.com"
+var accountPassword = "12222"
+accountCity = "dehradun"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity])
